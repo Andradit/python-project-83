06358135e46f4b528b4e09578375901cd5bc1858
Andradit <drakushev@mail.ru>
2025-03-20 22:17:25 +0300

    Add changes 20.03.2025

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -73,7 +73,9 @@ def checks(url_id):
     try:
         resp = requests.get(current_url[1])
         resp.raise_for_status()
-    except requests.ConnectionError or requests.RequestException:
+
+    # except requests.exceptions.HTTPError or requests.exceptions.ReadTimeout or requests.exceptions.ConnectionError or requests.exceptions.RequestException:
+    except requests.RequestException:
         flash('Произошла ошибка при проверке', 'danger')
         db.close_connection(conn)
         return redirect(url_for('url', url_id=url_id))
